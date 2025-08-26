11c9e00de0387fa4ece9fce9ddbbbab067be949f
tovedetered <skramer3@uvm.edu>
2025-08-26 09:32:40 -0400

    added cmake tools

--- init.lua
+++ init.lua
@@ -244,7 +244,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
+  { 'Civitasv/cmake-tools.nvim', opts = {} },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
